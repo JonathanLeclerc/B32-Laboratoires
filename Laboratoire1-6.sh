f707e653ff861080b9b12313fca67a82a2f7b9ad
Jonathan Leclerc <jonathanleclerc63@gmail.com>
2017-04-27 10:06:47 -0400

    Laboratoire 1 exercice 6 done!

---
+++ Laboratoire1-6.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+afficherMenu(){
+	echo "Appuyez sur q ou Q pour quitter"
+	echo "Appuyer sur a ou A pour afficher le contenu de root"
+	echo "Appuyez sur b ou B pour afficher le contenu de passwd"
+}
+rep=z
+
+until test $rep != z
+do
+	afficherMenu
+	read rep
+	case $rep in
+	a|A)
+	echo "Je suis dans a"
+	 sudo ls -la /root
+	;;
+	b|B)
+	 cat /etc/passwd
+	;;
+	q|Q)
+	 ;;
+	esac
+done
